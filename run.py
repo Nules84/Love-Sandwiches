0267f371e18418e6bc27ebb96bc2056b9ade833f
Nuala King <nualagallagher84@hotmail.com>
2022-03-27 19:20:35 +0000

    Add user request loop to repeat request for data until data provided …

--- run.py
+++ run.py
@@ -1,47 +1,46 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+""" Love Sandwiches """
 import gspread
 from google.oauth2.service_account import Credentials
 
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+@@ -14,18 +15,23 @@
 
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
 def get_sales_data():
+
     """
     Get Sales figures input from the user
     """
     print("Please enter sales data from the last market")
     print("Data should be six numbers, separated by commas.")
     print("Example: 10, 20, 30, 40, 50, 60\n")
+    while True:
+        print("Please enter sales data from the last market")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10, 20, 30, 40, 50, 60\n")
 
-data = sales.get_all_values()
-print(f'The data provided is {input("Enter your data here: ")}')
+        data_str = input("Enter your data here: ")
 
-print(data) 
-get_sales_data()
+    data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
 
-print(f'The data provided is {input("Enter your data here: ")}')
-    or if there aren't exactly 6 values
-    """
-    try:
-        [int(value) for value in values]
-        if len(values) != 6:
-            raise ValueError(
-                f"Exactly 6 values required, you provided {len(values)}"
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+        if validate_data(sales_data):
+            print("Data is valid")
+            break
+
+    return sales_data
+
+
+def validate_data(values):
+@@ -42,6 +48,9 @@ def validate_data(values):
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        get_sales_data()
+        return False
+
+    return True
 
 
 get_sales_data()
+data = get_sales_data()
