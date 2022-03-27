d5bc6220cfbcb38af04f2e56e47c0ff2f59c841f
Nuala King <nualagallagher84@hotmail.com>
2022-03-27 19:22:20 +0000

    Retrieve latest strock numbers from spreadsheet

--- run.py
+++ run.py
@@ -1,61 +1,38 @@
 """ Love Sandwiches """
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
-@@ -14,18 +15,23 @@
-
-
-def get_sales_data():
-    """
-    Get Sales figures input from the user
-    Get sales figures input from the user.
-    Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
-    by commas. The loop will repeatedly request data, until it is valid.
-    """
-    while True:
-        print("Please enter sales data from the last market")
-        print("Please enter sales data from the last market.")
-        print("Data should be six numbers, separated by commas.")
-        print("Example: 10, 20, 30, 40, 50, 60\n")
-        print("Example: 10,20,30,40,50,60\n")
-
-        data_str = input("Enter your data here: ")
-
-        sales_data = data_str.split(",")
-
-        if validate_data(sales_data):
-            print("Data is valid")
-            print("Data is valid!")
-            break
-
-    return sales_data
-
-
-def validate_data(values):
-    """
-    Insdie the try, converts all string values into integers
-    Inside the try, converts all string values into integers.
-    Raises ValueError if strings cannot be converted into int,
-    or if there aren't exactly 6 values
-    or if there aren't exactly 6 values.
-    """
-    try:
-        [int(value) for value in values]
-@@ -53,4 +56,16 @@ def validate_data(values):
-    return True
-
-
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with the list data provided
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet("sales")
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+@@ -65,13 +66,29 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet updated successfully.\n")
 
+def calculate_surplus_data(sales_row):
+    """
+    Compare sales with stock and calculate the surplus for each item type. 
+    the surplus is definied as the sales figure subtracted from the stock:
+    -Postitive surplus indicates waste
+    - Negative surplus indicated extra made when stock was sold out. 
+    """
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+    print("Calculating suplus data...|n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+    main()
+     
+print("welcome to Love Sandwiches Data Automation")
+main()
